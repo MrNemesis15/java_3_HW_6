51644e4671bec54da383a7dd34138a08f2866ca9
Alex <zall99971@gmail.com>
2021-10-28 13:08:44 +0300

    HW 6

---
+++ src/main/java/org/example/MainApp.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.logging.Logger;
+
+public class MainApp {
+    private static final Logger logger = Logger.getLogger (MainApp.class.getName ());
+    public static void main(String[] args) {
+
+    }
+}
